cf54dbc3482d40a4e2f340ae4bbf71c81ac94957
Rayan Wilbert <rayan.rmw@gmail.com>
2022-09-21 15:48:54 -0300

    Setup  public files

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,15 @@ import { routes } from './routes';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set('views', path.join(__dirname, '..', 'public', 'html'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/pages/client', (request, response) => {
+  return response.render('client.html')
+});
+
 const http = createServer(app);
 const io = new Server(http);
 
