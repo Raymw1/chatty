2ff80965dfa64a4ef058de7f668c977fd1b07002
Rayan Wilbert <rayan.rmw@gmail.com>
2022-09-21 15:43:58 -0300

    Add socket.io for WebSocket

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
 import express from 'express';
-import "./database";
-import { routes } from "./routes";
+import path from 'path';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+
+import './database';
+import { routes } from './routes';
 
 const app = express();
+
+const http = createServer(app);
+const io = new Server(http);
+
+io.on('connection', (socket: Socket) => {
+  console.log('Connected', socket.id);
+});
+
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('Go to http://localhost:3333/'));
+http.listen(3333, () => console.log('Go to http://localhost:3333/'));
