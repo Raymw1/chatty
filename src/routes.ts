d0b7e7dd70f9ea4589f02437b2aadee4fb999b7c
Rayan Wilbert <rayan.rmw@gmail.com>
2022-09-20 23:31:21 -0300

    Listing messages by userId

--- src/routes.ts
+++ src/routes.ts
@@ -10,7 +10,10 @@ const usersController = new UsersController();
 const messagesController = new MessagesController();
 
 routes.post('/settings', settingsController.create);
+
 routes.post('/users', usersController.create);
+
+routes.get('/messages/:id', messagesController.showByUser);
 routes.post('/messages', messagesController.create);
 
 export { routes };
