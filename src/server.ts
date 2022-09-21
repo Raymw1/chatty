43c10191ebf6fbe35014fa65f0118136fa58460d
Rayan Wilbert <rayan.rmw@gmail.com>
2022-09-21 16:11:47 -0300

    Begin WebSocket connection between server and client

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,4 @@
-import express from 'express';
-import path from 'path';
-import { createServer } from 'http';
-import { Server, Socket } from 'socket.io';
-
-import './database';
-import { routes } from './routes';
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, '..', 'public')));
-app.set('views', path.join(__dirname, '..', 'public', 'html'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-app.get('/pages/client', (request, response) => {
-  return response.render('client.html')
-});
-
-const http = createServer(app);
-const io = new Server(http);
-
-io.on('connection', (socket: Socket) => {
-  console.log('Connected', socket.id);
-});
-
-app.use(express.json());
-app.use(routes);
+import { http } from './http';
+import "./websocket/client";
 
 http.listen(3333, () => console.log('Go to http://localhost:3333/'));
